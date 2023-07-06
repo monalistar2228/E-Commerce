6653bfe72b7c370e0a4236170d786a323626e263
monalistar2228 <Monagadhekar007@gmail.com>
2023-07-06 23:48:24 +0530

    Up to logger done in UserController

--- src/main/java/com/bikkadit/ecommerce/controller/UserController.java
+++ src/main/java/com/bikkadit/ecommerce/controller/UserController.java
@@ -17,6 +17,8 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserController {
 
+
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
 
@@ -26,12 +28,14 @@ public class UserController {
      * @author Monali
      * @apiNote create user
      * @param  userDto
-     * @return
+     * @return User
      */
     @PostMapping(value = "/user")
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
 
+        logger.info("Request initiated for user service to create users");
         UserDto createUser = userService.createUser(userDto);
+        logger.info("Request completed for user service to create users");
         return new ResponseEntity<UserDto>(createUser, HttpStatus.CREATED);
 
     }
@@ -43,12 +47,14 @@ public class UserController {
      * @apiNote update User
      * @param userDto
      * @param userId
-     * @return
+     * @return updated user
      */
     @PutMapping(value = "/user/{userId}")
     public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable String userId){
 
+        logger.info("Request initiated for user service to update user");
         UserDto updatedUser = this.userService.updatedUser(userDto, userId);
+        logger.info("Request completed for user service to update user");
         return new ResponseEntity<UserDto>(updatedUser,HttpStatus.CREATED);
     }
 
@@ -58,12 +64,14 @@ public class UserController {
      * @author Monali
      * @apiNote Delete User by Id
      * @param userId
-     * @returnafd
+     * @return
      */
     @DeleteMapping(value = "/users/{userId}")
     public ResponseEntity<ApiResponse> deleteUser(@PathVariable String userId){
 
+        logger.info("Request initiated for user service to delete user");
         userService.deleteUser(userId);
+        logger.info("Request completed for user service to delete user");
         return new ResponseEntity(new ApiResponse("Delete user successfully", true),HttpStatus.OK);
 
     }
@@ -72,13 +80,15 @@ public class UserController {
     /**
      * @author Monali
      * @apiNote get all users
-     * @return
+     * @return List of all Users
      */
     @GetMapping(value = "/users")
     public ResponseEntity<List<UserDto>> getAllUsers(){
 
+        logger.info("Request initiated for user service to get all users");
         List<UserDto> allUsers = userService.getAllUser();
 
+        logger.info("Request Completed for user service to get all users");
         return new ResponseEntity<List<UserDto>>(allUsers,HttpStatus.OK);
     }
 
@@ -88,15 +98,31 @@ public class UserController {
      * @author Monali
      * @apiNote get single user by id
      * @param userId
-     * @return
+     * @return User by id
      */
     @GetMapping(value = "/users/{userId}")
     public ResponseEntity<UserDto> getUserById(@PathVariable String  userId)
     {
+        logger.info("Request initiated for user service to get user with id");
         UserDto userDto = userService.getUserById(userId);
+        logger.info("Request completed for user service to get user with id");
         return new ResponseEntity<UserDto>(userDto,HttpStatus.OK);
 
     }
 
+    /**
+     * @author Monali
+     * apiNote get user by email
+     * @param email
+     * @return User by email
+     */
+    @GetMapping("/email/{email}")
+   ResponseEntity<UserDto>getUserByEmail(@PathVariable String email)
+    {
+        logger.info("Request initiated for user service to get user with email id {}",email);
+       UserDto userDto1 =  this.userService.getUserByEmail(email);
+        logger.info("Request Completed for user service to get user with email id {}",email);
+        return new ResponseEntity<UserDto>(userDto1,HttpStatus.OK);
+    }
 
 }
