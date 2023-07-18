018a4c0d89c25178efd7cd8a268dd14341d09edc
monalistar2228 <Monagadhekar007@gmail.com>
2023-07-18 15:52:33 +0530

    Validation in UserDto is done

--- src/main/java/com/bikkadit/ecommerce/dto/UserDto.java
+++ src/main/java/com/bikkadit/ecommerce/dto/UserDto.java
@@ -1,6 +1,12 @@
 package com.bikkadit.ecommerce.dto;
 
+import com.bikkadit.ecommerce.validate.ImageNameValid;
 import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -10,11 +16,25 @@ import lombok.*;
 public class UserDto {
 
     private String userId;
+
+    @Size(min=3, max = 15,message = "Invalid User name !!")
     private String name;
+
+//    @Email(message = "Invalid User email !!")
+    @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\\.[a-zA-z]{2,3}$", message = "Invalid email")
+    @NotBlank(message = "Email is required !!")
     private String email;
+
+    @NotBlank(message = "Password is required !!")
     private String password;
+
+    @Size(min = 4,max = 6,message = "Invalid gender !!")
     private String gender;
+
+    @NotBlank(message = "Write something about yourself !!")
     private String about;
+
+    @ImageNameValid()
     private String imageName;
 
 
