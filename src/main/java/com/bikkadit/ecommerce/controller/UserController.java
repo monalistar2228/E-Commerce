a8b56fa7386d39bb6e01975415377e4f1db90c55
monalistar2228 <Monagadhekar007@gmail.com>
2023-07-18 23:40:50 +0530

    User model sorting done

--- src/main/java/com/bikkadit/ecommerce/controller/UserController.java
+++ src/main/java/com/bikkadit/ecommerce/controller/UserController.java
@@ -87,14 +87,16 @@ public class UserController {
     @GetMapping(value = "/users")
     public ResponseEntity<List<UserDto>> getAllUsers(
             @RequestParam(value = "pageNumber",defaultValue = "0",required = false) int pageNumber,
-            @RequestParam(value = "pageSize",defaultValue = "10",required = false) int pageSize){
+            @RequestParam(value = "pageSize",defaultValue = "10",required = false) int pageSize,
+            @RequestParam(value = "sortBy",defaultValue = "name",required = false) String sortBy,
+            @RequestParam(value = "sortDir",defaultValue = "asc",required = false) String sortDir){
 
         logger.info("Request initiated for user service to get all users");
 
-        List<UserDto> allUsers = userService.getAllUser(pageNumber,pageSize);
+        List<UserDto> allUsers = userService.getAllUser(pageNumber,pageSize,sortBy,sortDir);
 
         logger.info("Request Completed for user service to get all users");
-        return new ResponseEntity<>(userService.getAllUser(pageNumber,pageSize),HttpStatus.OK);
+        return new ResponseEntity<>(userService.getAllUser(pageNumber,pageSize,sortBy,sortDir),HttpStatus.OK);
     }
 
     // get single user by id
